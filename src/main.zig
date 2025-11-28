1147395099a504b1bb2691431799816fb42bd437
MyJules <mudryk200@gmail.com>
2025-11-28 18:43:48 +0100

    Adjustments

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,44 @@
 const std = @import("std");
 const ranges = @import("ranges.zig");
 
+fn isEven1(x: i32) bool { return @mod(x, 2) == 0; }
+fn square1(x: i32) i32 { return x * x; }
 fn isEven(x: usize) bool { return x % 2 == 0; }
 fn square(x: usize) usize { return x * x; }
+fn lessThen(x: usize) bool { return x < 1000; }
 
 pub fn main() void {
-    var range = ranges.range(0, 20);
-
+    const range = ranges.Range(usize).init(0, 20);
     var it = range
         .filter(isEven)
-        .map(square);
+        .map(square)
+        .filter(lessThen);
 
+    std.debug.print("Range: \n", .{});
     while (it.next()) |v| {
-        std.debug.print("{d}\n", .{v});
+        std.debug.print("{d} ", .{v});
+    }
+
+    std.debug.print("\n", .{});
+    std.debug.print("````````````````", .{});
+    std.debug.print("\n", .{});
+
+    const array = [_]i32{1, 2, 3, 4, 5, 6, 7, 8};
+    var arrayRange = ranges.ArrayRange(i32).init(&array);
+    var it1 = arrayRange
+        .map(square1);
+
+    std.debug.print("ArrayRange: \n", .{});
+    while (it1.next()) |v| {
+        std.debug.print("{d} ", .{v});
+    }
+
+    std.debug.print("\n", .{});
+    std.debug.print("````````````````", .{});
+    std.debug.print("\n", .{});
+
+    for (array, 0..) |value, idx| {
+        std.debug.print("arr[{}] = {}\n", .{idx, value});
     }
 }
 
--- src/ranges.zig
+++ src/ranges.zig
@@ -1,9 +1,74 @@
 const std = @import("std");
 
-//
-// Generic erased iterator
-//
-pub fn Iterator(comptime T: type) type {
+pub fn ArrayRange(comptime T: type) type {
+    return struct {
+        arr: []const T,
+        index: usize,
+
+        pub fn init(arr: []const T) @This() {
+            return .{ .arr = arr, .index = 0 };
+        }
+
+        pub fn next(self: *@This()) ?T {
+            if (self.index >= self.arr.len) return null;
+            const v = self.arr[self.index];
+            self.index += 1;
+            return v;
+        }
+
+        pub fn map(self: *const @This(), comptime F: anytype) MapIterator(@This(), F) {
+            return MapIterator(@This(), F).init(self.*);
+        }
+
+        pub fn filter(self: *const @This(), comptime P: anytype) FilterIterator(@This(), P) {
+            return FilterIterator(@This(), P).init(self.*);
+        }
+    };
+}
+
+pub fn Range(comptime T: type) type {
+    return struct {
+        start: T,
+        end: T,
+
+        pub fn init(start: usize, end: usize) @This() {
+            return @This() {
+                .start = start,
+                .end = end,
+            };
+        }
+
+        fn nextRaw(self: *@This()) ?usize {
+            if (self.start >= self.end) return null;
+            const v = self.start;
+            self.start += 1;
+            return v;
+        }
+
+        pub fn next(self: *@This()) ?usize {
+            return self.nextRaw();
+        }
+
+        fn nextAdapter(ctx: *anyopaque) ?usize {
+            const p: *@This() = @ptrCast(@alignCast(@alignOf(ctx)));
+            return p.next();
+        }
+
+        pub fn toIter(self: *@This()) Iterator(usize) {
+            return .{ .nextFn = nextAdapter, .ctx = self };
+        }
+
+        pub fn map(self: *const @This(), comptime F: anytype) MapIterator(@This(), F) {
+            return MapIterator(@This(), F).init(self.*);
+        }
+
+        pub fn filter(self: *const @This(), comptime P: anytype) FilterIterator(@This(), P) {
+            return FilterIterator(@This(), P).init(self.*);
+        }
+    };
+}
+
+fn Iterator(comptime T: type) type {
     return struct {
         nextFn: *const fn (ctx: *anyopaque) ?T,
         ctx: *anyopaque,
@@ -14,59 +79,13 @@ pub fn Iterator(comptime T: type) type {
     };
 }
 
-//
-// TypeInfo helper
-//
 fn getFnInfo(comptime F: anytype) @TypeOf(@typeInfo(@TypeOf(F)).@"fn") {
     const ti = @typeInfo(@TypeOf(F));
     if (ti != .@"fn") @compileError("expected function");
     return ti.@"fn";
 }
 
-//
-// Range iterator
-//
-pub fn range(start: usize, end: usize) Range {
-    return Range{ .start = start, .end = end };
-}
-
-pub const Range = struct {
-    start: usize,
-    end: usize,
-
-    fn nextRaw(self: *Range) ?usize {
-        if (self.start >= self.end) return null;
-        const v = self.start;
-        self.start += 1;
-        return v;
-    }
-
-    pub fn next(self: *@This()) ?usize {
-        return self.nextRaw();
-    }
-
-    fn nextAdapter(ctx: *anyopaque) ?usize {
-        const p: *Range = @ptrCast(@alignCast(@alignOf(ctx)));
-        return p.next();
-    }
-
-    pub fn toIter(self: *Range) Iterator(usize) {
-        return .{ .nextFn = nextAdapter, .ctx = self };
-    }
-
-    pub fn map(self: *const Range, comptime F: anytype) MapIterator(Range, F) {
-        return MapIterator(Range, F).init(self.*);
-    }
-
-    pub fn filter(self: *const Range, comptime P: anytype) FilterIterator(Range, P) {
-        return FilterIterator(Range, P).init(self.*);
-    }
-};
-
-//
-// Map iterator
-//
-pub fn MapIterator(comptime Inner: type, comptime F: anytype) type {
+fn MapIterator(comptime Inner: type, comptime F: anytype) type {
     const info = getFnInfo(F);
     const Out = info.return_type.?;
 
@@ -92,10 +111,7 @@ pub fn MapIterator(comptime Inner: type, comptime F: anytype) type {
     };
 }
 
-//
-// Filter iterator
-//
-pub fn FilterIterator(comptime Inner: type, comptime P: anytype) type {
+fn FilterIterator(comptime Inner: type, comptime P: anytype) type {
     const info = getFnInfo(P);
     const T = info.params[0].type.?;
 
